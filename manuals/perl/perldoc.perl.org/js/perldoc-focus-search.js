530c55a780d03388398b5818debd15a480963424
Daniel Ruul <danielruul78@gmail.com>
2025-09-23 12:57:49 +1000

    Revert "First Install"
    This reverts commit 9f58f018d5d6a21e231470fff6fc1d2bc21cac85.

--- manuals/perl/perldoc.perl.org/js/perldoc-focus-search.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('keydown', function (event) {
-  if ((event.key === 's' || event.key === 'S')
-      && !event.ctrlKey && !event.altKey && !event.metaKey
-      && document.activeElement.tagName.toLowerCase() !== 'input') {
-    document.getElementById('search-input').focus();
-    event.preventDefault();
-  }
-});
